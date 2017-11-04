6cee2588342fa83fade1a7fe2942f84399bed695
Zoran Kustrimovic <zoki925@gmail.com>
2017-11-04 16:05:02 +0100

    design home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="bower_components/ngToast/dist/ngToast.min.css">
     <link rel="stylesheet" href="partials/home/home.css">
     <link rel="stylesheet" href="partials/nav-menu/nav-menu.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
