b6890ac77020c61e787a04db827abcc3685ac5d8
misa95 <misa95kovic@gmail.com>
2017-11-05 08:22:01 +0100

    Zoraaan

--- app/Http/Controllers/OpeningController.php
+++ app/Http/Controllers/OpeningController.php
@@ -41,6 +41,7 @@ class OpeningController extends Controller
 		$opening = Opening::with('applications.expert', 'applications.answers')->where('id', $id)->first();
 		$opening->applications->transform(function ($app) {
 			$app->answers->load('question.test');
+			$app->expert->description;
 			$tests = $app->answers->groupBy(function ($answer) {
 				$test = 'Test' . $answer->question->test->queue;
 				return $test;
