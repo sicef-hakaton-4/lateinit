712e4f5064bbdfa2bc38ed85f99d72453577cf2d
Darko Kostic <kostic.darko95@hotmail.com>
2017-11-05 10:25:32 +0100

    alksd

--- public/partials/find-a-job/find-a-job.controller.js
+++ public/partials/find-a-job/find-a-job.controller.js
@@ -3,27 +3,28 @@ angular
     .controller('FindAJobCtrl', FindAJobCtrl);
 
 function FindAJobCtrl($stateParams, $scope, FindAJobService, $state, $uibModal, ngToast) {
-    console.log($stateParams.fromLogIn);
-    if($stateParams.fromLogIn.pera == 1) {
-        var modal = $uibModal.open({
-            animation: true,
-            templateUrl: 'partials/recension-modal/recension-modal.html',
-            controller: 'RecensionModal',
-            backdrop: true,
-            resolve: {
-                company: function () {
-                    return $stateParams.fromLogIn;
+    if($stateParams.fromLogIn) {
+        if($stateParams.fromLogIn.pera == 1) {
+            var modal = $uibModal.open({
+                animation: true,
+                templateUrl: 'partials/recension-modal/recension-modal.html',
+                controller: 'RecensionModal',
+                backdrop: true,
+                resolve: {
+                    company: function () {
+                        return $stateParams.fromLogIn;
+                    }
                 }
-            }
-        });
-
-        modal.result.then(function () {
-            ngToast.success({
-                content: "Thank you for your time"
             });
-        }, function (response) {
-            console.log(response);
-        });
+
+            modal.result.then(function () {
+                ngToast.success({
+                    content: "Thank you for your time"
+                });
+            }, function (response) {
+                console.log(response);
+            });
+        }
     }
     FindAJobService.getData().then(function(response) {
         $scope.jobs = response.entity;
