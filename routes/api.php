65dc2e4f53b3431f1530529b5fb38fb28b534a22
misa95 <misa95kovic@gmail.com>
2017-11-05 01:22:26 +0100

    Nastavak

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,22 @@ Route::group(['middleware' => 'jwt.auth'], function () {
 
 
 
+	//		-- Basic CRUD --
+
+	Route::post('put/{model}', 'BaseController@create'); // CREATE
+
+	Route::get('get/{model}', 'BaseController@readAll'); // READ LIST
+
+	Route::get('get/{model}/{id}', 'BaseController@readSingle'); // READ SINGLE
+
+	Route::get('patch/{model}/initialize/{id}', 'BaseController@initializePatch'); // LOAD DATA FOR UPDATE FORM
+
+	Route::post('patch/{model}/{id}', 'BaseController@update'); // UPDATE
+
+	Route::get('delete/{model}/{id}', 'BaseController@delete'); // DELETE
+
+
+
 
 	//		-- Openings --
 
@@ -24,19 +40,7 @@ Route::group(['middleware' => 'jwt.auth'], function () {
 
 
 
-//		-- Basic CRUD --
 
-Route::post('put/{model}', 'BaseController@create'); // CREATE
-
-Route::get('get/{model}', 'BaseController@readAll'); // READ LIST
-
-Route::get('get/{model}/{id}', 'BaseController@readSingle'); // READ SINGLE
-
-Route::get('patch/{model}/initialize/{id}', 'BaseController@initializePatch'); // LOAD DATA FOR UPDATE FORM
-
-Route::post('patch/{model}/{id}', 'BaseController@update'); // UPDATE
-
-Route::get('delete/{model}/{id}', 'BaseController@delete'); // DELETE
 
 
 
